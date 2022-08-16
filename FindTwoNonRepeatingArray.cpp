3f4d9d36b469d975a974b7d9e9a350398001407b
Younis Rahman <younisrahman007@gmail.com>
2022-08-16 21:52:19 +0600

    FindNonRepeatingArray

--- FindTwoNonRepeatingArray.cpp
+++ FindTwoNonRepeatingArray.cpp
@@ -4,14 +4,33 @@
 using namespace std;
 int main()
 {
-    int n, a, b, c, d;
-    int arr[] = {5, 4, 1, 4, 3, 5, 1};
-    n = 7;
+    int n, a = 0, b = 0, c, d;
+    int arr[] = {5, 4, 1, 4, 3, 5, 1, 2};
+    n = 8;
     int res = 0;
     for (int i = 0; i < n; i++)
     {
         res = res ^ arr[i];
     }
-    cout << res << endl;
+
+    /* Get the rightmost set bit in set_bit_no */
+    // int set_bit_no = res & ~(res - 1);
+    int set_bit_no = res & ~(res - 1);
+    /* Now divide elements in two sets by
+    comparing rightmost set bit of Xor with bit
+    at same position in each element. */
+    for (int i = 0; i < n; i++)
+    {
+
+        /*Xor of first set */
+        if (arr[i] & set_bit_no)
+            a = a ^ arr[i];
+        /*Xor of second set*/
+        else
+        {
+            b = b ^ arr[i];
+        }
+    }
+    cout << a << " " << b << endl;
     return 0;
 }
