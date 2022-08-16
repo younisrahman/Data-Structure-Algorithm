c71ee8a5c3cc27815f49295c3f1c238c902740c5
Younis Rahman <younisrahman007@gmail.com>
2022-08-16 22:00:28 +0600

    FindNonRepeatingArray

--- FindNonRepeatingArray2.cpp
+++ FindNonRepeatingArray2.cpp
@@ -1,17 +1,47 @@
-// find the non repeating number in an array while every element in repeting thrice
+// CPP program to find unique element where
+// every element appears k times except one
 #include <bits/stdc++.h>
-//#include<iostream>
 using namespace std;
+
+int findUnique(unsigned int a[], int n, int k)
+{
+    // Create a count array to store count of
+    // numbers that have a particular bit set.
+    // count[i] stores count of array elements
+    // with i-th bit set.
+    int INT_SIZE = 8 * sizeof(unsigned int);
+    int count[INT_SIZE];
+    memset(count, 0, sizeof(count));
+
+    // AND(bitwise) each element of the array
+    // with each set digit (one at a time)
+    // to get the count of set bits at each
+    // position
+    for (int i = 0; i < INT_SIZE; i++)
+        for (int j = 0; j < n; j++)
+            if ((a[j] & (1 << i)) != 0)
+                count[i] += 1;
+
+    // Now consider all bits whose count is
+    // not multiple of k to form the required
+    // number.
+    unsigned res = 0;
+    for (int i = 0; i < INT_SIZE; i++)
+        res += (count[i] % k) * (1 << i);
+
+    // Before returning the res we need
+    // to check the occurrence of that
+    // unique element and divide it
+    res = res / (n % k);
+    return res;
+}
+
+// Driver Code
 int main()
 {
-    int n, a, b, c, d;
-    int arr[] = {5, 4, 1, 4, 3, 5, 1};
-    n = 7;
-    int res = 0;
-    for (int i = 0; i < n; i++)
-    {
-        res = res ^ arr[i];
-    }
-    cout << res << endl;
+    unsigned int a[] = {6, 2, 5, 2, 2, 6, 6};
+    int n = sizeof(a) / sizeof(a[0]);
+    int k = 3;
+    cout << findUnique(a, n, k);
     return 0;
 }
