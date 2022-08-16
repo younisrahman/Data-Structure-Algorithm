26a83995bd88e6aaf7c9028b7017419ddba8fa4a
Younis Rahman <younisrahman007@gmail.com>
2022-08-16 21:53:16 +0600

    FindNonRepeatingArray

---
+++ FindNonRepeatingArray2.cpp
@@ -0,0 +1,17 @@
+// find the non repeating number in an array while every element in repeting thrice
+#include <bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+int main()
+{
+    int n, a, b, c, d;
+    int arr[] = {5, 4, 1, 4, 3, 5, 1};
+    n = 7;
+    int res = 0;
+    for (int i = 0; i < n; i++)
+    {
+        res = res ^ arr[i];
+    }
+    cout << res << endl;
+    return 0;
+}
