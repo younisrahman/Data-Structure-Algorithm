c61164f82419afe26a44a27244cd36036674329e
Younis Rahman <younisrahman007@gmail.com>
2022-08-18 00:31:30 +0600

    SieveOfEratosthenes.cpp

---
+++ Mathematics&NumberTheoryAlgorithms/SieveOfEratosthenes.cpp
@@ -0,0 +1,16 @@
+// SieveOfEratosthenes.cpp
+#include <bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+int main()
+{
+    int n, a, b, c, d;
+    vector<int> vector;
+    set<int> set;
+    cin >> n;
+    while (n--)
+    {
+        cin >> a >> b >> c >> d;
+    }
+    return 0;
+}
