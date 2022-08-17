4d9f531cf47713e275e2524734af25eb3fab5797
Younis Rahman <younisrahman007@gmail.com>
2022-08-18 01:05:46 +0600

    SieveOfEratosthenes

--- Mathematics&NumberTheoryAlgorithms/SieveOfEratosthenes.cpp
+++ Mathematics&NumberTheoryAlgorithms/SieveOfEratosthenes.cpp
@@ -2,15 +2,47 @@
 #include <bits/stdc++.h>
 //#include<iostream>
 using namespace std;
+bool *SieveOfEratosthenes(bool *arr, int n)
+{
+    for (int i = 0; i <= n; i++)
+    {
+        arr[i] = true;
+    }
+    arr[0] = arr[1] = false;
+    // for (int i = 2; i * i < n; i++)
+    // {
+    //     if (arr[i])
+    //     {
+    //         for (int j = 2 * i; j <= n; j += i)
+    //         {
+    //             arr[j] = false;
+    //         }
+    //     }
+    // }
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (arr[i])
+        {
+            for (int j = 2 * i; j <= n; j += i)
+            {
+                arr[j] = false;
+            }
+        }
+    }
+
+    return arr;
+}
 int main()
 {
     int n, a, b, c, d;
     vector<int> vector;
     set<int> set;
     cin >> n;
-    while (n--)
+    bool prime[n + 1];
+    bool *ptr = SieveOfEratosthenes(prime, n);
+    for (int i = 0; i <= n; i++)
     {
-        cin >> a >> b >> c >> d;
+        cout << i << " is " << prime[i] << endl;
     }
     return 0;
 }
