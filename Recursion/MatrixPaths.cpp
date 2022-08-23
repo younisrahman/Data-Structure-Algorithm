7286a173e4f794d5065743b9d111368c3d7835e9
Younis Rahman <younisrahman007@gmail.com>
2022-08-23 21:38:45 +0600

    Matrix Paths

---
+++ Recursion/MatrixPaths.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+int main()
+{
+    int n, a, b, c, d;
+    vector<int> vector;
+    set<int> set;
+    cin >> n;
+    while (n--)
+    {
+        cin >> a >> b >> c >> d;
+    }
+    return 0;
+}
