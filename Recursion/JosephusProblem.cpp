27757ea59a3ceab4cd30018a2727f0e2b4349c5d
Younis Rahman <younisrahman007@gmail.com>
2022-08-26 12:53:20 +0600

    Josephus Problem

--- Recursion/JosephusProblem.cpp
+++ Recursion/JosephusProblem.cpp
@@ -1,6 +1,14 @@
 #include <bits/stdc++.h>
 //#include<iostream>
 using namespace std;
+int josephusProblem(int n, int k)
+{
+    if (n == 1)
+    {
+        return 0;
+    }
+    return (josephusProblem(n - 1, k) + k) % n;
+}
 int main()
 {
     int n, a, b, c, d;
@@ -9,7 +17,8 @@ int main()
     cin >> n;
     while (n--)
     {
-        cin >> a >> b >> c >> d;
+        cin >> a >> b;
+        std::cout << josephusProblem(a, b) << std::endl;
     }
     return 0;
 }
