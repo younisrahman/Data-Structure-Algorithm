914666cf9a17af70fa9dae09f5ea157a47a81b8d
Younis Rahman <younisrahman007@gmail.com>
2022-09-04 22:20:23 +0600

    Permutations of a String

--- Recursion/PermutationsOfAString.cpp
+++ Recursion/PermutationsOfAString.cpp
@@ -1,6 +1,25 @@
 #include <bits/stdc++.h>
 //#include<iostream>
 using namespace std;
+void permutation(string s, int l, int r)
+{
+    if (l == r)
+    {
+        std::cout << s << std::endl;
+    }
+    for (int i = l; i < r; i++)
+    {
+        char c = s[i];
+        s[i] = s[l];
+        s[l] = c;
+
+        permutation(s, l + 1, r);
+
+        char ch = s[i];
+        s[i] = s[l];
+        s[l] = ch;
+    }
+}
 int main()
 {
     int n, a, b, c, d;
@@ -9,7 +28,9 @@ int main()
     cin >> n;
     while (n--)
     {
-        cin >> a >> b >> c >> d;
+        string s;
+        cin >> s;
+        permutation(s, 0, s.length());
     }
     return 0;
 }
