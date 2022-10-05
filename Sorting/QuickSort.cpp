e7e7641b6a806c9cb8d0a588609bbe53f7a6386a
Younis Rahman <younisrahman007@gmail.com>
2022-10-05 16:39:33 +0600

    quick sort

--- Sorting/QuickSort.cpp
+++ Sorting/QuickSort.cpp
@@ -2,62 +2,38 @@
 #include <iostream>
 using namespace std;
 
-int partition(int arr[], int start, int end)
+int partition(int arr[], int l, int h)
 {
-
-    int pivot = arr[start];
-
-    int count = 0;
-    for (int i = start + 1; i <= end; i++)
+    int pivot = arr[l];
+    int i = l;
+    int j = h;
+    while (i < j)
     {
-        if (arr[i] <= pivot)
-            count++;
-    }
-
-    // Giving pivot element its correct position
-    int pivotIndex = start + count;
-    swap(arr[pivotIndex], arr[start]);
-
-    // Sorting left and right parts of the pivot element
-    int i = start, j = end;
-
-    while (i < pivotIndex && j > pivotIndex)
-    {
-
         while (arr[i] <= pivot)
         {
             i++;
         }
-
         while (arr[j] > pivot)
         {
             j--;
         }
-
-        if (i < pivotIndex && j > pivotIndex)
+        if (i < j)
         {
-            swap(arr[i++], arr[j--]);
+            swap(arr[i], arr[j]);
         }
     }
-
-    return pivotIndex;
+    swap(arr[j], arr[l]);
+    return j;
 }
 
-void quickSort(int arr[], int start, int end)
+void quickSort(int arr[], int l, int h)
 {
-
-    // base case
-    if (start >= end)
-        return;
-
-    // partitioning the array
-    int p = partition(arr, start, end);
-
-    // Sorting the left part
-    quickSort(arr, start, p - 1);
-
-    // Sorting the right part
-    quickSort(arr, p + 1, end);
+    if (l < h)
+    {
+        int pivot = partition(arr, l, h);
+        quickSort(arr, l, pivot - 1);
+        quickSort(arr, pivot + 1, h);
+    }
 }
 
 int main()
