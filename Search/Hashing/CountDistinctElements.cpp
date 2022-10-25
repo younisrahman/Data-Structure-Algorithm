834cb14f3aac625d2246a69e17192e1814fadfa1
Younis Rahman <younisrahman007@gmail.com>
2022-10-26 01:12:15 +0600

    Has

---
+++ Search/Hashing/CountDistinctElements.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define FastIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+using namespace std;
+int main()
+{
+    FastIO;
+    int arr[] = {5, 10, 15, 5, 5, 4, 10, 4, 15};
+
+    unordered_set<int> s;
+    for (int i = 0; i < 9; i++)
+    {
+        s.insert(arr[i]);
+    }
+    std::cout << s.size() << std::endl;
+    return 0;
+}
