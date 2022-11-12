6a4798b43fa2ddf7f3585216cd20b5826daf07e0
Younis Rahman <younisrahman007@gmail.com>
2022-11-12 22:23:30 +0600

    heapyfy

--- HeapDataStructure/Heapify.cpp
+++ HeapDataStructure/Heapify.cpp
@@ -1,21 +1,47 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 #define ll long long int
 #define FastIO                        \
     ios_base::sync_with_stdio(false); \
     cin.tie(0);                       \
     cout.tie(0);
 using namespace std;
+void heapify(vector<int> &a, int n, int i)
+{
+    int largest = i;
+    int l = 2 * i;
+    int r = 2 * i + 1;
+    if (l <= n && a[l] > a[largest])
+    {
+        largest = l;
+    }
+    if (r <= n && a[r] > a[largest])
+    {
+        largest = r;
+    }
+    if (largest != i)
+    {
+        swap(a[i], a[largest]);
+        heapify(a, n, largest);
+    }
+}
+void buildheap(vector<int> &a, int n)
+{
+    for (int i = n / 2; i > 0; i--)
+    {
+        heapify(a, n, i);
+    }
+}
 int main()
 {
     FastIO;
-    int t;
-    cin >> t;
-    vector<int> vector;
-    set<int> set;
-    while (t--)
+    // vector<int> v = {40, 10, 30, 50, 60, 15};
+    vector<int> v = {50, 20, 40, 5, 10, 30};
+    buildheap(v, 6);
+    for (int i = 0; i < v.size(); i++)
     {
-        int n;
-        cin >> n;
+        std::cout << v[i] << " ";
     }
+    std::cout << " " << std::endl;
     return 0;
 }
