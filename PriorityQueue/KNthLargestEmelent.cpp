284f388032cf76a9bd3a010e93c4c5887e97f5be
Younis Rahman <younisrahman007@gmail.com>
2022-11-15 02:00:05 +0600

    kth largest element

--- PriorityQueue/KNthLargestEmelent.cpp
+++ PriorityQueue/KNthLargestEmelent.cpp
@@ -9,9 +9,21 @@ int main()
 {
     FastIO;
     vector<int> v = {3, 5, 7, 12, 66, 98, 43, 79, 32, 89};
-    int k = 3;
+    int k = 4;
 
     priority_queue<int> qa;
-
+    for (int i = 0; i < k; i++)
+    {
+        qa.push(-1 * v[i]);
+    }
+    for (int i = k; i < v.size(); i++)
+    {
+        if (v[i] > (qa.top() * -1))
+        {
+            qa.pop();
+            qa.push(-1 * v[i]);
+        }
+    }
+    std::cout << "Kth maximum element is " << qa.top() * -1 << std::endl;
     return 0;
 }
