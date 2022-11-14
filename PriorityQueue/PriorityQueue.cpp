14361d972ed68335d40de33913af8931e9b44d23
Younis Rahman <younisrahman007@gmail.com>
2022-11-15 01:38:09 +0600

    PriorityQueue

---
+++ PriorityQueue/PriorityQueue.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define FastIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+using namespace std;
+int main()
+{
+    FastIO;
+    int t;
+    cin >> t;
+    vector<int> vector;
+    set<int> set;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+    }
+    return 0;
+}
