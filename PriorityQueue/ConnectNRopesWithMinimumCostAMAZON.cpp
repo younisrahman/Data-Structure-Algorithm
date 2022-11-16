51b873480f27932c85ea7fb45d2903ce47f284f3
Younis Rahman <younisrahman007@gmail.com>
2022-11-16 13:22:06 +0600

    Connect N Ropes with minimum cost

--- PriorityQueue/ConnectNRopesWithMinimumCostAMAZON.cpp
+++ PriorityQueue/ConnectNRopesWithMinimumCostAMAZON.cpp
@@ -5,17 +5,32 @@
     cin.tie(0);                       \
     cout.tie(0);
 using namespace std;
+
+int connectRope(vector<int> &v)
+{
+    priority_queue<int> queue;
+    for (int i = 0; i < v.size(); i++)
+    {
+        queue.push(v[i] * -1);
+    }
+
+    int ans = 0;
+    while (queue.size() > 1)
+    {
+        int first = queue.top() * -1;
+        queue.pop();
+        int second = queue.top() * -1;
+        queue.pop();
+        int sum = first + second;
+        ans += sum;
+        queue.push(sum * -1);
+    }
+    return ans;
+}
 int main()
 {
     FastIO;
-    int t;
-    cin >> t;
-    vector<int> vector;
-    set<int> set;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-    }
+    vector<int> v = {2, 5, 4, 8, 6, 9};
+    std::cout << connectRope(v) << std::endl;
     return 0;
 }
